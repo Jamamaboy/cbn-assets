9614ea71c284ca54cbbcce0dafd2b885f75d8f60
Jamamaboy <putthipong.sowork@gmail.com>
2026-08-17 19:04:22 +0700

    _next/static for thaitidcha build static-3

--- _next/static/quBOWurcAFxiUJlJqX6bT/_buildManifest.js
+++ _next/static/quBOWurcAFxiUJlJqX6bT/_buildManifest.js
@@ -3,7 +3,7 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/gh/Jamamaboy/cbn-assets@static-2/_next/:path+",
+        "source": "/gh/Jamamaboy/cbn-assets@static-3/_next/:path+",
         "destination": "/_next/:path+"
       }
     ],
